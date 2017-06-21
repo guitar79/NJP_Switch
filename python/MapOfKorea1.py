3dd99c6de3b2e805bab54183a4ad988db1ef4a46
Kiehyun Kevin Park <guitar79@naver.com>
2017-06-21 10:05:41 +0900

    교수님들께 1차 보낸파일

--- python/MapOfKorea1.py
+++ python/MapOfKorea1.py
@@ -1,27 +1,30 @@
 from mpl_toolkits.basemap import Basemap
-import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
+import numpy as np
 
 # create new figure, axes instances.
 fig=plt.figure()
 ax=fig.add_axes([0.1,0.1,0.8,0.8])
-
 # setup mercator map projection.
 m = Basemap(llcrnrlon=111.,llcrnrlat=25.,urcrnrlon=145.,urcrnrlat=50.,\
             rsphere=(6378137.00,6356752.3142),\
             resolution='l',projection='merc',\
             lat_0=40.,lon_0=-20.,lat_ts=20.)
-def plot_rec(m, lower_left, upper_left, lower_right, upper_right):
-    upper_left = (127, 34)
-    upper_right= (135, 44)
-    plot_rec(m, lower_left, upper_left, lower_right, upper_right)
 
+#m.drawline(llcrnrlon=127,llcrnrlat=34,urcrnrlon=135,urcrnrlat=44,linewidth=2,color='b')
+
+lats = [34,44,44,34,34] 
+
+lons = [127, 127, 135, 135, 127] 
+
+x, y = m(lons, lats) # forgot this line 
+m.plot(x, y, 'D-', markersize=0, linewidth=2, color='k', markerfacecolor='b') 
 
 m.drawcoastlines()
 m.drawcountries()
 m.fillcontinents()
-
+#print country.attributes['name_long'], earth_colors.next()
+#plt.annotate('Korea', xy=(1, 1), ha="center")
 # draw parallels
 m.drawparallels(np.arange(10,90,10),labels=[1,1,0,1])
 # draw meridians
